3039561f75580b2657c8c2ab4a32ff4c639bdbc4
Zach Mitchell <zmitchell@fastmail.com>
2023-11-05 17:45:39 -0700

    Build and cache deps separately

--- flake.nix
+++ flake.nix
@@ -1,51 +1,80 @@
 {
-    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     inputs.flake-utils.url = "github:numtide/flake-utils";
     inputs.rust-overlay.url = "github:oxalica/rust-overlay";
+    inputs.crane.url = "github:ipetkov/crane";
+    inputs.crane.inputs.nixpkgs.follows = "nixpkgs";
 
-
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, crane, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
-        version = pkgs.lib.attrByPath ["package" "version"] "0.0" (builtins.fromTOML (builtins.readFile ./Cargo.toml));
-      in
-      {
-        packages.default = pkgs.rustPlatform.buildRustPackage {
-          pname = "discord_bot";
-          version = version;
+        craneLib = (crane.mkLib pkgs).overrideToolchain pkgs.rust-bin.stable.latest.default;
 
-          src = builtins.path {path = ./.; name = "discord_bot_src"; };
-          cargoLock.lockFile = ./Cargo.lock;
+        # Rust source
+        src = craneLib.cleanCargoSource (craneLib.path ./.);
 
-          nativeBuildInputs = with pkgs; [
-            rustc
-            cargo
-            cachix
-          ];
-          
+        # Common arguments for building/testing
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+
           buildInputs = with pkgs; [
-            # None yet for Linux
-          ] ++ lib.optional hostPlatform.isDarwin [
+            # Additional buildInputs for Linux
+          ] ++ lib.optionals pkgs.stdenv.isDarwin [
+            # Additional buildInputs for macOS
+            libiconv
             darwin.apple_sdk.frameworks.SystemConfiguration
           ];
+        };
 
+        # Build *just* the crate dependencies so we can reuse
+        # all of that work (e.g. via cachix) when running in CI
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        # Build the actual crate itself, reusing the dependency
+        # artifacts from above.
+        discordBot = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+          # Don't run tests here, we'll run them in checks
+          doCheck = false;
           meta = {
             description = "Discord bot for the Boulder Rust meetup Discord server";
             homepage = "https://github.com/boulderrust/discord_bot";
             license = with pkgs.lib.licenses; [mit asl20];
             maintainers = [];
           };
-        };
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [
-            self.packages.${system}.default
-          ];
+        });
+      in
+      {
+        checks = {
+          inherit discordBot;
 
-          shellHoook = ''
-            source .env;
-          '';
+          # Run clippy, denying warnings
+          run-clippy = craneLib.cargoClippy (commonArgs // {
+            inherit cargoArtifacts;
+            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+          });
+
+          # Check formatting
+          run-fmt = craneLib.cargoFmt {
+            inherit src;
+          };
+
+          # Run tests with cargo-nextest
+          run-nextest = craneLib.cargoNextest (commonArgs // {
+            inherit cargoArtifacts;
+            partitions = 1;
+            partitionType = "count";
+          });
+        };
+        packages.default = discordBot;
+        devShells.default = craneLib.devShell {
+          checks = self.checks.${system};
+          packages = with pkgs; [
+            cachix
+          ];
         };
       });
 }

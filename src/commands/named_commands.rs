39593ab6cda74103fb29fc5539b3402cd7360cd9
Tyler Weaver <maybe@tylerjw.dev>
2023-11-08 19:56:13 -0700

    help message: pull requests welcome

--- src/commands/named_commands.rs
+++ src/commands/named_commands.rs
@@ -1,12 +1,25 @@
 //! This module dispatches on named commands, e.g. `!mycmd arg1 arg2`.
 
 use super::Error;
-use serenity::{model::prelude::Message, prelude::Context as SerenityContext};
+use serenity::{model::prelude::Message, prelude::Context as SerenityContext, utils::MessageBuilder};
+use tracing::error;
 
-pub async fn handle_named_command(_ctx: SerenityContext, _msg: Message) -> Result<(), Error> {
+pub async fn handle_named_command(ctx: SerenityContext, msg: Message) -> Result<(), Error> {
     // Call each command in sequence here.
-    // my_command(ctx.clone(), msg.clone()).await?;
+    help(ctx.clone(), msg.clone()).await?;
     todo!()
+}
+
+async fn help(ctx: SerenityContext, msg: Message) -> Result<(), Error> {
+    if msg.content.starts_with("!help") {
+        let response = MessageBuilder::new()
+            .push("Pull requests welcome, help not yet implemented: github.com/boulder-rust/discord_bot. Have fun!")
+            .build();
+        if let Err(err) = msg.channel_id.say(ctx.http, response).await {
+            error!(%err, "couldn't send reply");
+        }
+    }
+    Ok(())
 }
 
 // async fn my_command(ctx: SerenityContext, msg: Message) -> Result<(), Error> {
